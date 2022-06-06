095f44253c74fc146d052793aab0304dc80e0805
Jaedson Silva <contacanal798@gmail.com>
2022-06-06 20:42:43 -0300

    changing project version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author='Jaedson Silva',
     author_email='imunknowuser@protonmail.com',
     name='colorprynt',
-    version='0.1.0',
+    version='1.0.0',
     description='Print color text easily.',
     long_description=long_description,
     long_description_content_type='text/markdown',
