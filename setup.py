4e2bb5589ff450b502c4ae55365ce7d6eb3b2812
Jaedson Silva <contacanal798@gmail.com>
2022-06-06 20:33:21 -0300

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    author='Jaedson Silva',
+    author_email='imunknowuser@protonmail.com',
+    name='colorprynt',
+    version='0.1.0',
+    description='Print color text easily.',
+    packages=['colorprynt'],
+    license='MIT',
+    project_urls={
+        'Source code': 'https://github.com/jaedsonpys/colorprynt'
+    }
+)
