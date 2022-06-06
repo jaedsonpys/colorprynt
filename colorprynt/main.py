331bd08b7b0e4712d76716559842472df33d56a3
Jaedson Silva <contacanal798@gmail.com>
2022-06-06 19:53:18 -0300

    writing colors code in "file"

--- colorprynt/main.py
+++ colorprynt/main.py
@@ -7,3 +7,29 @@ PURPLE = '\033[35m'
 CYAN = '\033[36m'
 WHITE = '\033[37m'
 CLOSE = '\033[m'
+
+import sys
+from typing import TextIO
+
+def print(
+    value: str, 
+    end: str = '\n',
+    file: TextIO = sys.stdout,
+    color: str = 'white'
+) -> None:
+    if color == 'black':
+        file.write(BLACK)
+    elif color == 'red':
+        file.write(RED)
+    elif color == 'green':
+        file.write(GREEN)
+    elif color == 'yellow':
+        file.write(YELLOW)
+    elif color == 'blue':
+        file.write(BLUE)
+    elif color == 'purple':
+        file.write(PURPLE)
+    elif color == 'cyan':
+        file.write(CYAN)
+    elif color == 'white':
+        file.write(WHITE)
